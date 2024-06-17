3f0c9ab6c29cfe1856e28e01af7af5fd0479c065
Leereal <leereal08@ymail.com>
2024-06-17 07:12:06 +0200

    main branch is now for production only

--- backend/core/celery.py
+++ backend/core/celery.py
@@ -3,7 +3,7 @@ import os
 from celery import Celery
 from django.conf import settings
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings.development") #PRODUCTION ONLY
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings.production") #PRODUCTION ONLY
 
 app = Celery("microfinex")
 
